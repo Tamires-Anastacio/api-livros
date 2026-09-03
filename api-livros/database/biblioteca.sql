1eb8b7317a967bacec260b6015afdfb18ca1290b
Tamires-Anastacio <tamiresanastaciodefreitas283@gmail.com>
2026-09-03 08:41:48 -0300

    cria estrutura inicial da aplicacao

---
+++ api-livros/database/biblioteca.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03-Set-2026 às 13:30
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `biblioteca`
+--
+-- Erro ao ler a estrutura para a tabela biblioteca_db.biblioteca: #1146 - Tabela &#039;biblioteca_db.biblioteca&#039; não existe
+-- Erro ao ler dados para tabela biblioteca_db.biblioteca: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `biblioteca_db`.`biblioteca`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
